64dc2dcd7f9151dd94225a8bcbe06b7674251623
Amjad Elahi <amjadelahi07@gmail.com>
2024-07-31 11:42:29 +0300

    add multiplication function

--- bin/lab1.dart
+++ bin/lab1.dart
@@ -1,4 +1,6 @@
 import 'add.dart';
+import 'multi.dart';
 void main(List<String> arguments) {
   add(6,4);
+  multi(6,6);
 }
---
+++ bin/multi.dart
@@ -0,0 +1,4 @@
+multi(int num1, int num2){
+  int res = num1 * num2;
+  print(res);
+}
